476006495cf211d42f289920005e5dfcf1e3a4f0
Renato Lima <natenho@gmail.com>
2017-01-04 22:31:55 -0200

    Namespaces removed

--- ResiliencyTryHandler.cs
+++ ResiliencyTryHandler.cs
@@ -1,45 +1,42 @@
-﻿using System;
+using System;
 
-namespace Winsys.ApplicationFramework.Helpers
+/// <summary>
+/// Permite manipular o evento de cada tentativa da classe de <see cref="Resiliency"/>
+/// </summary>
+public class ResiliencyTryHandler<TException> where TException : Exception
 {
-    /// <summary>
-    /// Permite manipular o evento de cada tentativa da classe de <see cref="Resiliency"/>
-    /// </summary>
-    public class ResiliencyTryHandler<TException> where TException : Exception
-    {
-        #region Properties
+	#region Properties
 
-        /// <summary>
-        /// Opção para abortar o ciclo de tentativas
-        /// </summary>
-        public bool AbortRetry { get; set; }
+	/// <summary>
+	/// Opção para abortar o ciclo de tentativas
+	/// </summary>
+	public bool AbortRetry { get; set; }
 
-        /// <summary>
-        /// <see cref="Exception"/> a ser tratada
-        /// </summary>
-        public TException Exception { get; private set; }
+	/// <summary>
+	/// <see cref="Exception"/> a ser tratada
+	/// </summary>
+	public TException Exception { get; private set; }
 
-        /// <summary>
-        /// Identifca o número da tentativa atual
-        /// </summary>
-        public int CurrentTry { get; private set; }
+	/// <summary>
+	/// Identifca o número da tentativa atual
+	/// </summary>
+	public int CurrentTry { get; private set; }
 
-        #endregion
+	#endregion
 
-        #region Constructors
+	#region Constructors
 
-        /// <summary>
-        /// Instancia um manipulador de tentativa. É utilizado internamente
-        /// por <see cref="Resiliency"/> para permitir que o cliente altere o
-        /// comportamento do ciclo de tentativas
-        /// </summary>
-        public ResiliencyTryHandler(TException exception, int currentTry)
-        {
-            Exception = exception;
-            CurrentTry = currentTry;
-        }
+	/// <summary>
+	/// Instancia um manipulador de tentativa. É utilizado internamente
+	/// por <see cref="Resiliency"/> para permitir que o cliente altere o
+	/// comportamento do ciclo de tentativas
+	/// </summary>
+	public ResiliencyTryHandler(TException exception, int currentTry)
+	{
+		Exception = exception;
+		CurrentTry = currentTry;
+	}
 
-        #endregion
+	#endregion
 
-    }
 }
